816600188a67cac97538cb5f076db0126fe45b5f
levanse <levansepro@gmail.com>
2021-10-24 22:49:36 +0300

    lesson 15

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -44,4 +44,26 @@ class ArticleController extends Controller
         return redirect()
             ->route('articles.index');
     }
+
+    public function edit($id)
+    {
+        $article = Article::findOrFail($id);
+
+        return view('article.edit', compact('article'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $article = Article::findOrFail($id);
+        $data = $this->validate($request, [
+            'name' => 'required|unique:articles,name,' . $article->id,
+            'body' => 'required|min:100',
+        ]);
+
+        $article->fill($data);
+        $article->save();
+
+        return redirect()
+            ->route('articles.index');
+    }
 }
