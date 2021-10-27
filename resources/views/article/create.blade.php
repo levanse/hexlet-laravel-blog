92d20170545c3658c50ad67f907eb3e4b4443bb4
levanse <levansepro@gmail.com>
2021-10-27 16:46:27 +0300

    fix lesson 15

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -1,13 +1,3 @@
-@if ($errors->any())
-    <div>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-
 {{ Form::model($article, ['url' => route('articles.store')]) }}
     @include('article.form')
     {{ Form::submit('Сохранить') }}
