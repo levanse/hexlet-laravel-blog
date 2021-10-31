17e1a43bfc288492fb8869a2303a083d0ceb9d47
levanse <levansepro@gmail.com>
2021-10-31 19:29:30 +0200

    fix lesson 17

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ArticleRequest;
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articles = Article::paginate();
+
+        return view('article.index', compact('articles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $article = new Article();
+
+        return view('article.create', compact('article'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ArticleRequest $request)
+    {
+        $data = $request->validated();
+
+        $request->session()->flash('success', 'Статья успешно добавлена');
+
+        $article = new Article();
+        $article->fill($data);
+        $article->save();
+
+        return redirect()
+            ->route('articles.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Article $article)
+    {
+        return view('article.show', compact('article'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Article $article)
+    {
+        return view('article.edit', compact('article'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Article $article)
+    {
+        $data = $this->validate($request, [
+            'name' => 'required|unique:articles,name,' . $article->id,
+            'body' => 'required|min:100',
+        ]);
+
+        $request->session()->flash('success', 'Статья обновлена');
+
+        $article->fill($data);
+        $article->save();
+
+        return redirect()
+            ->route('articles.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request, Article $article)
+    {
+        if ($article) {
+            $article->delete();
+        }
+
+        $request->session()->flash('success', 'Статья удалена');
+
+        return redirect()->route('articles.index');
+    }
+}
